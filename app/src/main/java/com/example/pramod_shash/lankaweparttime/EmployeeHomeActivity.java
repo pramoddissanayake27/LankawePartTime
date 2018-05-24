2c04634243d43b2511bdc2e037113d1261c267b7
pramoddissanayake27 <pramoddissanayake27@gmail.com>
2018-05-24 21:19:35 +0530

    2nd commit

--- app/src/main/java/com/example/pramod_shash/lankaweparttime/EmployeeHomeActivity.java
+++ app/src/main/java/com/example/pramod_shash/lankaweparttime/EmployeeHomeActivity.java
@@ -3,16 +3,28 @@ package com.example.pramod_shash.lankaweparttime;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class EmployeeHomeActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private Button logOut;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,53 @@ public class EmployeeHomeActivity extends AppCompatActivity {
                Logout();
             }
         });
+
+        toolbar = (Toolbar)findViewById(R.id.toolbarEmployee);
+        toolbar.setTitle("Material drawer test");
+
+        //Mike Penz material drawer
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Pramod Dissanayake").withEmail("pramoddissanayake27@gmail.com").withIcon(getResources().getDrawable(R.drawable.profile))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Home");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("Settings");
+        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withIdentifier(3).withName("Jobs");
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1, item2,item3
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        switch (position){
+                            case 1 : break;
+                            case 2 : break;
+                            case 3 : break;
+                        }
+                        return true;
+                    }
+                })
+                .build();
     }
     private void Logout(){
         firebaseAuth.signOut();
