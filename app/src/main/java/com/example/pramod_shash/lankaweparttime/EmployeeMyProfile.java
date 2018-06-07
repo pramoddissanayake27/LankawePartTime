a36584bfaa75904e40ce9ff66bc05cacf64b1e19
unknown <piumali>
2018-06-07 14:24:14 +0530

    employe location getting

---
+++ app/src/main/java/com/example/pramod_shash/lankaweparttime/EmployeeMyProfile.java
@@ -0,0 +1,13 @@
+package com.example.pramod_shash.lankaweparttime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeMyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_my_profile);
+    }
+}
